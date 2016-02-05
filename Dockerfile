c9dc4155561492a3869ef4f47488924f5cd849cc
Chris Black <chris@circleci.com>
2016-02-04 17:14:51 -0800

    Creating a docker container for pager bot

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ruby:2.3.0
+MAINTAINER CircleCI sayhi@circleci.com
+
+ADD Gemfile /
+
+RUN gem install bundler
+RUN bundle install
---
+++ docker.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+
+SHORT_SHA=$(git rev-parse --short HEAD)
+DOCKER_TAG="0.1.${CIRCLE_BUILD_NUM}-${SHORT_SHA}"
+
+build_docker() {
+   DOCKER_BUILD_PATH=/tmp/pb-docker
+   mkdir -p ${DOCKER_BUILD_PATH}
+
+   cp Dockerfile ${DOCKER_BUILD_PATH}/
+   cp Gemfile ${DOCKER_BUILD_PATH}/
+
+   pushd ${DOCKER_BUILD_PATH}
+   docker build -t circleci/pagerbot:${DOCKER_TAG} .
+   popd
+}
+
+build_docker
